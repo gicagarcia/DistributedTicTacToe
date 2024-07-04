9ed1721a6731bc7663087f2b5b43603479ed77ce
User <gicagarcia58@gmail.com>
2024-07-04 16:59:48 -0300

    fix: changing class to fit logic

--- app/src/main/java/com/example/distributedtictactoe/Game.kt
+++ app/src/main/java/com/example/distributedtictactoe/Game.kt
@@ -1,8 +1,7 @@
 package com.example.distributedtictactoe
 
 data class Game(
-    val id: String = "",
-    val board: List<MutableList<String>> = List(3) { MutableList(3) { "" } },
+    val board: MutableList<String> = MutableList(9) { "" },
     var jogadorX: String = "jogadorX",
     var jogadorO: String = "jogador0",
     var turn: String = "jogadorX"
